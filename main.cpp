3cd53f10f81e136b39d07bca0c4b5d16a254d958
zapheroc <zapheroc@users.noreply.github.com>
2016-03-11 15:42:51 -0800

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,101 +1,37 @@
 #include <iostream>
-#include "CImg.h"
-#include <fstream>
-#include <cstdlib> // for the exit function
-#include <vector>
-#include <math.h>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-void createSnowCrash(string fileName, string outputName = "snowCrash.png");
-void extractSnowCrash(string fileName);
-void parseArguments(int, char *[], string &, string &);
+unsigned int Random(int nLow, int nHigh) {
+    return (rand() % (nHigh - nLow + 1)) + nLow;
+}
 
-int main(int argc, char *argv[]) {
+int getNumber() {
+    cout << "Please enter a number between 1 and 9: ";
+    int nineInput = 0;
+    cin >> nineInput;
+    return nineInput;
+}
 
-    if (argc > 2) {
-    string inputFile = "//empty\\";
-    string outputFile = "//empty\\";
-    parseArguments(argc, argv, inputFile, outputFile);
-        createSnowCrash(inputFile, outputFile);
+int main()
+{
+    int nineInput = getNumber();
+    if (nineInput > 0 && nineInput < 10) {
+        cout << "Good Job." << endl;
     }
     else {
-        extractSnowCrash(argv[1]);
+        cout << "That's not a number." << endl;
+        main();
     }
     return 0;
 }
 
-void extractSnowCrash(string fileName) {
+/*
+Typically, enumerations are used to index arrays. Super neat, because that way it's easier to tell what each element is.
 
-    using namespace cimg_library;
 
-    ofstream outData("decodedSnow.txt", ios::binary);
-    CImg<unsigned char> img(fileName.c_str());
-    for (unsigned int h = 0; h < img.height(); h++) {
-        for (unsigned int w = 0; w < img.width(); w++) {
-            outData << img(w, h, 2);
-        }
-    }
-}
-
-void createSnowCrash(string fileName, string outputName) {
-
-    using namespace cimg_library;
-
-    ifstream inData(fileName, ios::binary|ios::ate);
-
-    if (!inData) {
-        cerr << "Input File was not found.";
-        exit(1);
-    }
-
-    ifstream::pos_type pos = inData.tellg();
-    vector<char> result(pos);
-    inData.seekg(0, ios::beg);
-    inData.read(result.data(), pos);
-
-    unsigned int imageWidth = 0;
-    unsigned int imageHeight = 0;
-    unsigned int numberOfBytes = result.size();
-
-    imageWidth = round(sqrt(numberOfBytes));
-
-    if (imageWidth < 1)  {
-        imageWidth = 1;
-    }
-
-    imageHeight = imageWidth;
-    //Replace magic number 3 with RBG!
-    while (imageWidth * imageHeight < result.size()) {
-        imageHeight++;
-    }
-
-    CImg<unsigned char> img(imageWidth, imageHeight, 1, 3);
-    unsigned int arrayPos = 0;
-    for (unsigned int h = 0; h < imageHeight; h++) {
-        for (unsigned int w = 0; w < imageWidth; w++) {
-        if (arrayPos < numberOfBytes) {
-            img(w, h, 2) = result.at(arrayPos);
-        }
-        arrayPos++;
-        }
-    }
-    img.save_png(outputName.c_str());
-}
-
-void parseArguments(int argc, char *argv[], string &inputFile, string &outputFile) {
-
-    for (int arg = 0; arg < argc; arg++) {
-        if (strcmp(argv[arg], "-i") == 0) {
-            inputFile = argv[arg + 1];
-        }
-        else if (strcmp(argv[arg], "-o") == 0) {
-            outputFile = argv[arg + 1];
-        }
-    }
-
-    if (inputFile == "//empty\\" || outputFile == "//empty\\") {
-        cerr << "No file given for input/output. Use -i for input, and -o for output" << endl;
-        exit(1);
-    }
-}
+*/
