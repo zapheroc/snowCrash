c4b38665c60891bb950545f22715c47978b6bc91
Andrew Price <agprice@umail.ucsb.edu>
2019-04-04 21:34:58 -0700

    Cleaned up the compile scripts slightly.

--- compileLinux.sh
+++ compileLinux.sh
@@ -1,1 +1,1 @@
-g++ src/*.cpp -std=c++11 -lX11 -lz -lpng -Wall -I./include/ -lpng -lpthread -O2 -o snowCrashGCCWORKS
+g++ src/*.cpp -std=c++11 -lX11 -lz -lpng -Wall -I./include/ -lpng -lpthread -O3 -o snowCrashGCCWORKS
