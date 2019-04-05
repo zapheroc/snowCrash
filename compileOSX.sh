b48aa5f3f612f231508946b4fdd457ee1bb277ae
woona <woona@luna.local>
2019-04-04 22:16:07 -0700

    Fixed OSX compiling. Added some crunched files to test multiplatform support.

--- compileOSX.sh
+++ compileOSX.sh
@@ -1,1 +1,1 @@
-g++-5 src/*.cpp -std=c++11 -Wall -I./include/ -I/usr/local/include/ -I/opt/X11/include/libpng15/ -L/opt/X11/lib/ -lX11 -D cimg_use_png -lpng -O3 -o snowCrashGCCWORKS
+g++ src/*.cpp -std=c++11 -Wall -I./include/ -I/usr/X11R6/include -L/usr/X11R6/lib -I/opt/X11/include/libpng15/ -lX11 -D cimg_use_png -lpng -O3 -o snowCrash
